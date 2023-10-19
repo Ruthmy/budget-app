18aad94cf8e1cc61057973a13d6e7652e11ba432
Ruth Abreu <ruthmy17@gmail.com>
2023-10-19 11:12:51 -0600

    Revert "Feature/set up project"

--- db/migrate/20231017205613_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :users do |t|
-      t.string :name, null: false, limit: 50, unique: true
-
-      t.timestamps
-    end
-  end
-end
