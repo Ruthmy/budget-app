08b4d26c713ad297e39e52aaac2aa2599b88ee70
Ruthmy <ruthmy17@gmail.com>
2023-10-19 17:50:45 -0600

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_003548) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_230447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,9 +35,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_003548) do
 
   create_table "payments", force: :cascade do |t|
     t.bigint "author_id", null: false
+    t.bigint "group_id", null: false
     t.string "name", limit: 100, null: false
     t.float "amount", null: false
-    t.bigint "group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_payments_on_author_id"
@@ -48,6 +48,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_003548) do
     t.string "name", limit: 50, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "group_payments", "groups"
