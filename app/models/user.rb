cf5423df3d30f4b0fb5f3c03414bf7d2eca0fb2a
Ruthmy <ruthmy17@gmail.com>
2023-10-19 17:51:10 -0600

    Modified 'user.rb' -model- - Added devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable
+
   has_many :payments, foreign_key: :author_id, dependent: :destroy
   has_many :groups, foreign_key: :user_id, dependent: :destroy
 
