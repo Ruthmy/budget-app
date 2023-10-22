3231a550f609b170055c55ab320d1c356b4daebb
Ruthmy <ruthmy17@gmail.com>
2023-10-21 18:33:04 -0600

    Added the shoalda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,3 +67,9 @@ RSpec.configure do |config|
     Rails.logger.level = 0
   end
 end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
